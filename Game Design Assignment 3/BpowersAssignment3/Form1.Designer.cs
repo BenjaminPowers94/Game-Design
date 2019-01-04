9cf874c2a29447c2f6fb51e2a44f8bfbd431329d
Benjamin Powers <41301379+BenjaminPowers94@users.noreply.github.com>
2019-01-04 15:12:58 -0500

    Add files via upload

---
+++ Game Design Assignment 3/BpowersAssignment3/Form1.Designer.cs
@@ -0,0 +1,243 @@
+﻿namespace BpowersAssignment3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.topLeftBtn = new System.Windows.Forms.Button();
+            this.topMiddleBtn = new System.Windows.Forms.Button();
+            this.topRightBtn = new System.Windows.Forms.Button();
+            this.middleLeftBtn = new System.Windows.Forms.Button();
+            this.middleMiddleBtn = new System.Windows.Forms.Button();
+            this.middleRightBtn = new System.Windows.Forms.Button();
+            this.bottomLeftBtn = new System.Windows.Forms.Button();
+            this.bottomMiddleBtn = new System.Windows.Forms.Button();
+            this.bottomRightBtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.newGameBtn = new System.Windows.Forms.Button();
+            this.resetBtn = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.oWinLbl = new System.Windows.Forms.Label();
+            this.xWinLbl = new System.Windows.Forms.Label();
+            this.drawLbl = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // topLeftBtn
+            // 
+            this.topLeftBtn.Location = new System.Drawing.Point(138, 84);
+            this.topLeftBtn.Name = "topLeftBtn";
+            this.topLeftBtn.Size = new System.Drawing.Size(95, 88);
+            this.topLeftBtn.TabIndex = 0;
+            this.topLeftBtn.UseVisualStyleBackColor = true;
+            this.topLeftBtn.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // topMiddleBtn
+            // 
+            this.topMiddleBtn.Location = new System.Drawing.Point(239, 84);
+            this.topMiddleBtn.Name = "topMiddleBtn";
+            this.topMiddleBtn.Size = new System.Drawing.Size(95, 88);
+            this.topMiddleBtn.TabIndex = 1;
+            this.topMiddleBtn.UseVisualStyleBackColor = true;
+            this.topMiddleBtn.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // topRightBtn
+            // 
+            this.topRightBtn.Location = new System.Drawing.Point(340, 84);
+            this.topRightBtn.Name = "topRightBtn";
+            this.topRightBtn.Size = new System.Drawing.Size(95, 88);
+            this.topRightBtn.TabIndex = 2;
+            this.topRightBtn.UseVisualStyleBackColor = true;
+            this.topRightBtn.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // middleLeftBtn
+            // 
+            this.middleLeftBtn.Location = new System.Drawing.Point(138, 178);
+            this.middleLeftBtn.Name = "middleLeftBtn";
+            this.middleLeftBtn.Size = new System.Drawing.Size(95, 88);
+            this.middleLeftBtn.TabIndex = 3;
+            this.middleLeftBtn.UseVisualStyleBackColor = true;
+            this.middleLeftBtn.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // middleMiddleBtn
+            // 
+            this.middleMiddleBtn.Location = new System.Drawing.Point(239, 178);
+            this.middleMiddleBtn.Name = "middleMiddleBtn";
+            this.middleMiddleBtn.Size = new System.Drawing.Size(95, 88);
+            this.middleMiddleBtn.TabIndex = 4;
+            this.middleMiddleBtn.UseVisualStyleBackColor = true;
+            this.middleMiddleBtn.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // middleRightBtn
+            // 
+            this.middleRightBtn.Location = new System.Drawing.Point(340, 178);
+            this.middleRightBtn.Name = "middleRightBtn";
+            this.middleRightBtn.Size = new System.Drawing.Size(95, 88);
+            this.middleRightBtn.TabIndex = 5;
+            this.middleRightBtn.UseVisualStyleBackColor = true;
+            this.middleRightBtn.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // bottomLeftBtn
+            // 
+            this.bottomLeftBtn.Location = new System.Drawing.Point(138, 272);
+            this.bottomLeftBtn.Name = "bottomLeftBtn";
+            this.bottomLeftBtn.Size = new System.Drawing.Size(95, 88);
+            this.bottomLeftBtn.TabIndex = 6;
+            this.bottomLeftBtn.UseVisualStyleBackColor = true;
+            this.bottomLeftBtn.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // bottomMiddleBtn
+            // 
+            this.bottomMiddleBtn.Location = new System.Drawing.Point(239, 272);
+            this.bottomMiddleBtn.Name = "bottomMiddleBtn";
+            this.bottomMiddleBtn.Size = new System.Drawing.Size(95, 88);
+            this.bottomMiddleBtn.TabIndex = 7;
+            this.bottomMiddleBtn.UseVisualStyleBackColor = true;
+            this.bottomMiddleBtn.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // bottomRightBtn
+            // 
+            this.bottomRightBtn.Location = new System.Drawing.Point(340, 272);
+            this.bottomRightBtn.Name = "bottomRightBtn";
+            this.bottomRightBtn.Size = new System.Drawing.Size(95, 88);
+            this.bottomRightBtn.TabIndex = 8;
+            this.bottomRightBtn.UseVisualStyleBackColor = true;
+            this.bottomRightBtn.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(215, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(138, 24);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "TIC TAC TOE";
+            // 
+            // newGameBtn
+            // 
+            this.newGameBtn.Location = new System.Drawing.Point(29, 429);
+            this.newGameBtn.Name = "newGameBtn";
+            this.newGameBtn.Size = new System.Drawing.Size(107, 49);
+            this.newGameBtn.TabIndex = 10;
+            this.newGameBtn.Text = "New Game";
+            this.newGameBtn.UseVisualStyleBackColor = true;
+            this.newGameBtn.Click += new System.EventHandler(this.newGameBtn_Click);
+            // 
+            // resetBtn
+            // 
+            this.resetBtn.Location = new System.Drawing.Point(142, 429);
+            this.resetBtn.Name = "resetBtn";
+            this.resetBtn.Size = new System.Drawing.Size(107, 49);
+            this.resetBtn.TabIndex = 11;
+            this.resetBtn.Text = "Reset Scores";
+            this.resetBtn.UseVisualStyleBackColor = true;
+            this.resetBtn.Click += new System.EventHandler(this.resetBtn_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(442, 429);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(107, 49);
+            this.exitButton.TabIndex = 12;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // oWinLbl
+            // 
+            this.oWinLbl.AutoSize = true;
+            this.oWinLbl.Location = new System.Drawing.Point(26, 118);
+            this.oWinLbl.Name = "oWinLbl";
+            this.oWinLbl.Size = new System.Drawing.Size(0, 13);
+            this.oWinLbl.TabIndex = 13;
+            // 
+            // xWinLbl
+            // 
+            this.xWinLbl.AutoSize = true;
+            this.xWinLbl.Location = new System.Drawing.Point(26, 212);
+            this.xWinLbl.Name = "xWinLbl";
+            this.xWinLbl.Size = new System.Drawing.Size(0, 13);
+            this.xWinLbl.TabIndex = 14;
+            // 
+            // drawLbl
+            // 
+            this.drawLbl.AutoSize = true;
+            this.drawLbl.Location = new System.Drawing.Point(26, 306);
+            this.drawLbl.Name = "drawLbl";
+            this.drawLbl.Size = new System.Drawing.Size(0, 13);
+            this.drawLbl.TabIndex = 15;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(572, 521);
+            this.Controls.Add(this.drawLbl);
+            this.Controls.Add(this.xWinLbl);
+            this.Controls.Add(this.oWinLbl);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.resetBtn);
+            this.Controls.Add(this.newGameBtn);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.bottomRightBtn);
+            this.Controls.Add(this.bottomMiddleBtn);
+            this.Controls.Add(this.bottomLeftBtn);
+            this.Controls.Add(this.middleRightBtn);
+            this.Controls.Add(this.middleMiddleBtn);
+            this.Controls.Add(this.middleLeftBtn);
+            this.Controls.Add(this.topRightBtn);
+            this.Controls.Add(this.topMiddleBtn);
+            this.Controls.Add(this.topLeftBtn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button topLeftBtn;
+        private System.Windows.Forms.Button topMiddleBtn;
+        private System.Windows.Forms.Button topRightBtn;
+        private System.Windows.Forms.Button middleLeftBtn;
+        private System.Windows.Forms.Button middleMiddleBtn;
+        private System.Windows.Forms.Button middleRightBtn;
+        private System.Windows.Forms.Button bottomLeftBtn;
+        private System.Windows.Forms.Button bottomMiddleBtn;
+        private System.Windows.Forms.Button bottomRightBtn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button newGameBtn;
+        private System.Windows.Forms.Button resetBtn;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.Label oWinLbl;
+        private System.Windows.Forms.Label xWinLbl;
+        private System.Windows.Forms.Label drawLbl;
+    }
+}
+
---
+++ Game Design Assignment 3/BpowersAssignment3/Form1.cs
@@ -0,0 +1,169 @@
+﻿//-----------------------------------
+// Ben Powers
+// Assignment 3 (Tic Tac Toe)
+// Game Design
+// Student Number 7986250
+// December 2nd 2018
+//-----------------------------------
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BpowersAssignment3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public int players = 2;
+        public int turns = 0;
+        public int x1 = 0;
+        public int o2 = 0;
+        public int tie = 0;
+
+        private void buttonClick(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            //if(button.Text == "")
+            if(button.BackgroundImage == null )
+            {
+                if (players % 2 == 0)
+                {
+                    button.Text = "X";
+                    button.BackgroundImage = Properties.Resources.ximage;
+                    players++;
+                    turns++;
+                }
+                else
+                {
+                    button.Text = "O";
+                    button.BackgroundImage = Properties.Resources.oimage;
+                    players++;
+                    turns++;
+                }
+                if (CheckForTieGame() == true)
+                {
+                    MessageBox.Show("Tie game");
+                    tie++;
+                    NewGame();
+                }
+                if (CheckWinner() == true)
+                {
+                    if(button.Text == "X")
+                    //if(button.BackgroundImage == Properties.Resources.ximage)
+                    {
+                        MessageBox.Show("X Won");
+                        x1++;
+                        NewGame();
+                    }
+                    else
+                    {
+                        MessageBox.Show("O Won");
+                        o2++;
+                        NewGame();
+                    }
+                }
+            }
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            xWinLbl.Text = "X: " + x1;
+            oWinLbl.Text = "O: " + o2;
+            drawLbl.Text = "Draws: " + tie;
+        }
+
+        void NewGame()
+        {
+            players = 2;
+            turns = 0;
+            topLeftBtn.Text = topMiddleBtn.Text = topRightBtn.Text = middleLeftBtn.Text = middleMiddleBtn.Text = middleRightBtn.Text = bottomLeftBtn.Text = bottomMiddleBtn.Text = bottomRightBtn.Text = "";
+            topLeftBtn.BackgroundImage = topMiddleBtn.BackgroundImage = topRightBtn.BackgroundImage = middleLeftBtn.BackgroundImage = middleMiddleBtn.BackgroundImage = middleRightBtn.BackgroundImage = bottomLeftBtn.BackgroundImage = bottomMiddleBtn.BackgroundImage = bottomRightBtn.BackgroundImage = null;
+            xWinLbl.Text = "X: " + x1;
+            oWinLbl.Text = "O: " + o2;
+            drawLbl.Text = "Draws: " + tie;
+        }
+
+        private void newGameBtn_Click(object sender, EventArgs e)
+        {
+            NewGame();
+        }
+
+        bool CheckForTieGame()
+        {
+            if ((turns == 9) &&CheckWinner()==false)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        bool CheckWinner()
+        {
+            // horizontal checks
+            if((topLeftBtn.Text == topMiddleBtn.Text) && (topMiddleBtn.Text == topRightBtn.Text) && topLeftBtn.Text != "") 
+            {
+                return true;
+            }
+            else if ((middleLeftBtn.Text == middleMiddleBtn.Text) && (middleMiddleBtn.Text == middleRightBtn.Text) && middleLeftBtn.Text != "") 
+            {
+                return true;
+            }
+            else if((bottomLeftBtn.Text == bottomMiddleBtn.Text) && (bottomMiddleBtn.Text == bottomRightBtn.Text) && bottomLeftBtn.Text != "") 
+            {
+                return true;
+            }
+
+            // vertical checks 
+            if ((topLeftBtn.Text == middleLeftBtn.Text) && (middleLeftBtn.Text == bottomLeftBtn.Text) && topLeftBtn.Text != "") 
+            {
+                return true;
+            }
+            else if ((topMiddleBtn.Text == middleMiddleBtn.Text) && (middleMiddleBtn.Text == bottomMiddleBtn.Text) && topMiddleBtn.Text != "") 
+            {
+                return true;
+            }
+            else if ((topRightBtn.Text == middleRightBtn.Text) && (middleRightBtn.Text == bottomRightBtn.Text) && topRightBtn.Text != "") 
+            {
+                return true;
+            }
+
+            // diagnol checks 
+            if ((topLeftBtn.Text == middleMiddleBtn.Text) && (middleMiddleBtn.Text == bottomRightBtn.Text) && topLeftBtn.Text != "") 
+            {
+                return true;
+            }
+            else if ((topRightBtn.Text == middleMiddleBtn.Text) && (middleMiddleBtn.Text == bottomLeftBtn.Text) && topRightBtn.Text != "") 
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private void resetBtn_Click(object sender, EventArgs e)
+        {
+            x1 = o2 = tie = 0;
+            NewGame();
+        }
+    }
+}
